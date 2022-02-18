3254f1d1a2e3426f27f8db0d441261f9e3fc294c
“Noah” <noah@simajji.com>
2022-02-18 15:54:58 +0800

    Applied prettier on the App.js

--- App.js
+++ App.js
@@ -1,78 +1,83 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Contents from './screens/Contents.js';
-import Profile from './screens/Profile.js';
-import Settings from './screens/Settings.js';
-import Login from './screens/Login.js';
-import React from 'react';
-import { MaterialCommunityIcons, Ionicons, AntDesign } from '@expo/vector-icons';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Contents from "./screens/Contents.js";
+import Profile from "./screens/Profile.js";
+import Settings from "./screens/Settings.js";
+import Login from "./screens/Login.js";
+import React from "react";
+import {
+  MaterialCommunityIcons,
+  Ionicons,
+  AntDesign,
+} from "@expo/vector-icons";
 import { LoginPhoto } from "./components/enlargeImage";
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
   return (
-      // Settings up the tab navigation(icon,name), set Settings as initial route for first register person
-      <Tab.Navigator
-      initialRouteName = "Settings"
-      >
-        <Tab.Screen
-          name="Home"
-          component={Contents}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={30} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarLabel: 'Profile',
-            tabBarIcon: ({ color }) => (
-              <AntDesign name="profile" color={color} size={30} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={Settings}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="ios-settings" color={color} size={30} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+    <Tab.Navigator initialRouteName="Settings">
+      <Tab.Screen
+        name="Home"
+        component={Contents}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="profile" color={color} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarLabel: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="ios-settings" color={color} size={30} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
 function App() {
   return (
-    // Using stack to split the login screen and after log in, nested navigation
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen options={{headerShown: false}} name="Login" component={Login} />
-        <Stack.Screen options={{
-        headerLeft: ()=>(
-          <LoginPhoto size={"small"}/>
-        ), 
-        headerTitle:"My Doggo App",
-        headerTitleStyle:{
-          fontFamily:'AmericanTypewriter-Bold',
-          fontSize:22
-        }
-        }}
-        name="Home" component={HomeScreen} />
+        {/* Probably need to have a splash screen to load things up */}
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen
+          options={{
+            headerLeft: () => <LoginPhoto size={"small"} />,
+            headerTitle: "My Doggo App",
+            headerTitleStyle: {
+              fontFamily: "AmericanTypewriter-Bold",
+              fontSize: 22,
+            },
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 export default App;
-
-
